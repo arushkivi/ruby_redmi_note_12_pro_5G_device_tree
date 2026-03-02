38607aad31e3e5855ad33cc28fe9b3cc5f7956fe
satvik patil <satvikpatil@satviks-MacBook-Air.local>
2026-03-02 16:03:24 +0530

    ruby: Fix device.mk conflicts and build script resilience

--- device.mk
+++ device.mk
@@ -4,36 +4,35 @@
 
 LOCAL_PATH := device/xiaomi/ruby
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a55
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# A/B
-AB_OTA_UPDATER := true
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
 # Virtual A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+ENABLE_VIRTUAL_AB := true
+AB_OTA_UPDATER := true
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# Soong Namespaces
-PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
-
+# Copy recovery files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/fstab.mt6877:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/fstab.mt6877 \
     $(LOCAL_PATH)/recovery/root/init.recovery.mt6877.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6877.rc
 
-# Device Identification
-PRODUCT_MODEL := Redmi Note 12 Pro 5G
-PRODUCT_NAME := twrp_ruby
-PRODUCT_DEVICE := ruby
-PRODUCT_BRAND := Redmi
-PRODUCT_MANUFACTURER := Xiaomi
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
